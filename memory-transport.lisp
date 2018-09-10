0202f5f0e5d9e7af96ac5f24d73f3a4811625d53
Aaron France <aaron.l.france@gmail.com>
2018-09-10 18:51:51 +0200

    Implement a finite state machine stub for in memory raft servers

--- memory-transport.lisp
+++ memory-transport.lisp
@@ -1,7 +1,11 @@
-(in-package :raft)
+(defpackage :raft/memory-transport
+  (:use :cl
+        :raft/state
+        :raft/fsm
+        :raft/transport))
+(in-package :raft/memory-transport)
 
-
-(defclass in-memory-transport ()
+(define-state-machine memory-transport :follower ()
   ((peers
     :initarg :peers
     :initform (make-hash-table)
@@ -10,11 +14,34 @@
     :initform (make-instance 'raft-state)
     :accessor raft-state)))
 
+(defun make-memory-transport ())
 
-(defmethod update-entries ((imt in-memory-transport) (ae raft/msgs::append-entries))
-  )
+(defmethod raft/fsm:state ((imt memory-transport))
+  (current-state (raft-state imt)))
 
-(defmethod append-entries ((imt in-memory-transport) server-id server-address (ae raft/msgs::append-entries))
-  (make-instance 'append-entries-response
-                 :term (> (term (raft-state imt) (term ae)))
-                 :success (update-entries imt ae)))
+(defmethod (setf raft/fsm:state) (state (imt memory-transport))
+  (setf (current-state (raft-state imt)) state))
+
+(define-state-handler memory-transport :follower (imt state (ae raft/msgs::append-entries))
+  ;;   Receiver implementation:
+  ;; 1. Reply false if term < currentTerm (§5.1)
+  ;; 2. Reply false if log doesn’t contain an entry at prevLogIndex
+  ;; whose term matches prevLogTerm (§5.3)
+  ;; 3. If an existing entry conflicts with a new one (same index
+  ;; but different terms), delete the existing entry and all that
+  ;; follow it (§5.3)
+  ;; 4. Append any new entries not already in the log
+  ;; 5. If leaderCommit > commitIndex, set commitIndex =
+  ;; min(leaderCommit, index of last new entry)
+  state)
+
+(define-state-handler memory-transport :leader (imt state (ae raft/msgs::append-entries))
+  ;; check if term > our term
+  ;; apply to memory- log, replicate to followers
+  state)
+
+(define-state-handler memory-transport :candidate (imt state (ae raft/msgs::append-entries))
+  state)
+
+(define-state-handler memory-transport :follower (imt state (ae raft/msgs:append-entries))
+  state)
