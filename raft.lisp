71ba5fce0f9f5306e92e8faa0e856d563a4c456a
Aaron France <aaron.l.france@gmail.com>
2018-09-10 23:59:30 +0200

    Drop spec comments

--- raft.lisp
+++ raft.lisp
@@ -26,21 +26,9 @@
   (setf (current-state (raft-state imt)) state))
 
 (define-state-handler raft :follower (imt state (ae raft/msgs:append-entries))
-  ;;   Receiver implementation:
-  ;; 1. Reply false if term < currentTerm (§5.1)
-  ;; 2. Reply false if log doesn’t contain an entry at prevLogIndex
-  ;; whose term matches prevLogTerm (§5.3)
-  ;; 3. If an existing entry conflicts with a new one (same index
-  ;; but different terms), delete the existing entry and all that
-  ;; follow it (§5.3)
-  ;; 4. Append any new entries not already in the log
-  ;; 5. If leaderCommit > commitIndex, set commitIndex =
-  ;; min(leaderCommit, index of last new entry)
   state)
 
 (define-state-handler raft :leader (imt state (ae raft/msgs:append-entries))
-  ;; check if term > our term
-  ;; apply to memory- log, replicate to followers
   state)
 
 (define-state-handler raft :candidate (imt state (ae raft/msgs:append-entries))
