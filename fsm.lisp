f40481cd824d5a2024cf75baf5838e12cf253dea
Aaron France <aaron.l.france@gmail.com>
2018-09-11 14:50:52 +0200

    Fix FSM logging

--- fsm.lisp
+++ fsm.lisp
@@ -19,7 +19,7 @@
 (defgeneric state-machine-event (state-machine state-machine-state event))
 
 (defmethod state-machine-event :around (state-machine state-machine-state event)
-  (log:debug "State machine: ~A in state: ~A handling event: ~A" state-machine state-machine state event))
+  (log:debug "State machine: ~A in state: ~A handling event: ~A" state-machine state-machine-state event))
 
 (defun apply-event (state-machine event)
   (multiple-value-bind (next-state recur-p)
