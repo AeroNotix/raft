e076478e88dfa83f5cb7b1be008e80e4dc319806
Aaron France <aaron.l.france@gmail.com>
2018-09-21 15:16:32 +0200

    Add documentation to the transport API

--- transport.lisp
+++ transport.lisp
@@ -25,16 +25,27 @@
     :initform (raft/serialization:make-basic-serializer)
     :accessor serializer)))
 
-(defgeneric connect (transport server-address))
+(defgeneric connect (transport server-address)
+  (:documentation "Connect to a peer"))
 
-(defgeneric disconnect (transport server-address))
+(defgeneric disconnect (transport server-address)
+  (:documentation "Disconnect from a peer"))
 
-(defgeneric rpc-channel (transport))
+(defgeneric rpc-channel (transport)
+  (:documentation "Return the primitive rpc channel that this
+  transport uses"))
 
-(defgeneric append-entries (transport server-address append-entry))
+(defgeneric append-entries (transport server-address append-entry)
+  (:documentation "Relay an AppendEntries RPC to the provided server
+  address over this transport"))
 
-(defgeneric request-vote (transport server-address request-vote))
+(defgeneric request-vote (transport server-address request-vote)
+  (:documentation "Relay a RequestVote RPC to the provided server
+  address over this transport"))
 
-(defgeneric request-vote-response (transport server-address request-vote-request))
+(defgeneric request-vote-response (transport server-address request-vote-request)
+  (:documentation "Relay a response to a RequestVote RPC to the
+  provided server address over this transport"))
 
-(defgeneric hangup (transport))
+(defgeneric hangup (transport)
+  (:documentation "Close this transport"))
