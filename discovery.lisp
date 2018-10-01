56b798e653431d414c4564d3416c37116db3abaf
Aaron France <aaron.l.france@gmail.com>
2018-10-02 01:12:15 +0200

    Add docstring

--- discovery.lisp
+++ discovery.lisp
@@ -6,6 +6,13 @@
    #:background-find-peers))
 (in-package :raft/discovery)
 
+#|
+
+Typically the discovery implementation will be tied to the transport
+implementation. I.e. the memory discovery mechanism relies on the
+transport's implementation. Doesn't have to be like this, however.
+
+|#
 
 (defclass discoverer () ())
 
