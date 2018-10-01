eaa027185955567b5644b82bc5f89d4a13a65a46
Aaron France <aaron.l.france@gmail.com>
2018-10-01 14:54:09 +0200

    Improve grammar in tests

--- tests/memory-raft.lisp
+++ tests/memory-raft.lisp
@@ -38,40 +38,40 @@
          (hangup-transport (compose
                             #'raft/transport:hangup
                             #'raft:transport)))
-    (ok (eq (length rafts) cluster-size) "Cluster size is as expected")
+    (ok (eq (length rafts) cluster-size) "Cluster size is as expected.")
     ;; XXX: checking implementation details
     (ok (eq (hash-table-count raft/memory-transport::*memory-transport-directory*) cluster-size)
-        "Memory transports register themselves with the memory transport directory")
+        "Memory transports register themselves with the memory transport directory.")
     (ok (every #'identity (mapcar validate-peer-count rafts))
-        "Every raft instance in the peer group knows about all the other peers")
+        "Every raft instance in the peer group knows about all the other peers.")
     (mapcar hangup-transport rafts)))
 
 (deftest followers-become-candidates-after-heartbeat-timeout
   (let ((raft (first (make-n-rafts 1))))
     (ok (raft:follower-p raft)
-        "All raft instances start in the follower state")
+        "All raft instances start in the follower state.")
     (raft/fsm:apply-event raft :heartbeat-timeout)
-    (ok (raft:candidate-p raft) "After a heartbeat timeout all followers become candidates")))
+    (ok (raft:candidate-p raft) "After a heartbeat timeout all followers become candidates.")))
 
 (deftest candidate-reverts-to-follower-upon-newer-term
   (let ((raft (first (make-n-rafts 1))))
     (ok (raft:follower-p raft)
-            "All raft instances start in the follower state")
+            "All raft instances start in the follower state.")
     (raft/fsm:apply-event raft :heartbeat-timeout)
     (ok (raft:candidate-p raft)
-        "After a heartbeat timeout all followers become candidates")
+        "After a heartbeat timeout all followers become candidates.")
     (raft/fsm:apply-event raft (make-instance 'raft/msgs:request-vote
                                               :last-log-term 1
                                               :last-log-index 1
                                               :candidate-id 1
                                               :term 10))
     (ok (raft:follower-p raft)
-        "Candidates revert to follower when receiving a request vote with a higher term")))
+        "Candidates revert to follower when receiving a request vote with a higher term.")))
 
 (deftest followers-reject-append-entries-with-old-terms
   (let ((raft (first (make-n-rafts 1))))
     (ok (raft:follower-p raft)
-        "All raft instances start in the follower state")
+        "All raft instances start in the follower state.")
     ;; -1 chosen because it's the first lowest impossible value that
     ;; should trigger rejection of AppendEntries RPC requests.
     (raft/fsm:apply-event raft (make-instance 'raft/msgs:append-entries
@@ -100,25 +100,25 @@
          (rafts (create-in-memory-cluster cluster-size))
          (leader (first rafts))
          (followers (rest rafts)))
-    (ok (eq (length rafts) cluster-size) "Cluster size is as expected")
+    (ok (eq (length rafts) cluster-size) "Cluster size is as expected.")
     (ok (eq (hash-table-count raft/memory-transport::*memory-transport-directory*) cluster-size)
-             "Memory transports register themselves with the memory transport directory")
+             "Memory transports register themselves with the memory transport directory.")
     (raft/fsm:apply-event leader :heartbeat-timeout)
     (ok (raft:candidate-p leader)
-             "After a heartbeat timeout, the raft instance should convert to candidate")
+             "After a heartbeat timeout, the raft instance should convert to candidate.")
     (let ((request-votes (mapcar recv-rpc-channel followers))
           (rvr (make-instance 'raft/msgs:request-vote-response :vote-granted t :term 1)))
       (ok (eq (length request-votes) (length followers))
-               "The leader should send each intended follower a RequestVote RPC")
+               "The leader should send each intended follower a RequestVote RPC.")
       (ok (every (funcall class-is 'raft/msgs:request-vote) request-votes)
-               "The only RPC in each intended follower should be a RequestVote from the intended leader")
+               "The only RPC in each intended follower should be a RequestVote from the intended leader.")
       (loop for follower in followers
          do
            (raft/fsm:apply-event leader rvr))
       (ok (raft:leader-p leader)
-          "After all followers have replied with a successful RequestVotes response, we are the leader")
+          "After all followers have replied with a successful RequestVotes response, we are the leader.")
       (ok (every #'raft:follower-p followers)
-          "After a leader is chosen, all other raft instances are still followers"))
+          "After a leader is chosen, all other raft instances are still followers."))
     (mapcar hangup-transport rafts)))
 
 (deftest competing-leader-elections
